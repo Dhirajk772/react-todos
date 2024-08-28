3e04222c9f8b37bff32a0ca62be48616c193f13b
Dhiraj Kumar <dhirajk772@gmail.com>
2024-08-28 23:49:40 +0530

    REACT-001 : react TODOs

---
+++ src/components/TodoTable.js
@@ -0,0 +1,29 @@
+import TodoRowItems from "./TodoRowItems"
+
+function TodoTable(props) {
+
+    return (
+        <table className='table table-hover'>
+            <thead>
+                <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Assigned</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.todos.map(todo => (
+                    <TodoRowItems
+                        key= {todo.rowNumber}
+                        rowNumber={todo.rowNumber}
+                        rowDescription={todo.rowDescription}
+                        rowAssigned={todo.rowAssigned}
+                        deleteTodo={props.deleteTodo}
+                    />
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TodoTable
